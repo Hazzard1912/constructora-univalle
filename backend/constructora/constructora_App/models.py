558dee971813bdc0d17ae64d4c29566ab5c76e5b
Oscar Cuaical <cuaical.oscar@correounivalle.edu.co>
2024-04-09 16:56:54 -0500

    Backend -> Se crea un SuperUser para acceder a django admin y se hacen las migraciones hacia supabase

--- backend/constructora/constructora_App/models.py
+++ backend/constructora/constructora_App/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# Django -Admin
+# Name = constructora
+# Password = DSconstructora
+# Esta es una version de prueba para una tabla
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+    
