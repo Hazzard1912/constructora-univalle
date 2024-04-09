3a2f07e87dd209e99058cc2cee979831cd5d3212
Oscar Cuaical <cuaical.oscar@correounivalle.edu.co>
2024-04-09 16:24:43 -0500

    Backend -> Se agrega RestFramework y se instalan modulos adicionales

--- backend/constructora/constructora/settings.py
+++ backend/constructora/constructora/settings.py
@@ -37,12 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'psycopg2',
+    'corsheaders',
+    'rest_framework',
     'constructora_App',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#para las peticiones al servidor desde otro servidor
+#util para las peticiones del servidor del frontend al servidor del backend
+CORS_ALLOWED_ORIGINS = []
