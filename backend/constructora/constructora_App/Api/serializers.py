0a588edc890dc34caaa0c958a2b141c4b84a8bb6
Oscar Cuaical <cuaical.oscar@correounivalle.edu.co>
2024-05-22 21:54:28 -0500

    Se modifica el id_type en serializers

--- backend/constructora/constructora_App/Api/serializers.py
+++ backend/constructora/constructora_App/Api/serializers.py
@@ -2,6 +2,16 @@ from rest_framework import serializers
 from ..models import UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
+   
+    ID_TYPE_CHOICES = [
+        ('DNI', 'C.C Cédula de Ciudadania'),
+        ('TI', 'T.I Tarjeta de Identidad'),
+        ('CE', 'Cédula de Extranjería'),
+        ('PAS', 'Pasaporte'),
+    ]
+
+    id_type = serializers.ChoiceField(choices=ID_TYPE_CHOICES)
+    
     class Meta:
         model = UserProfile
         fields = '__all__'
